3f1df4cd4399c8a3acc02f282eb115527ae30750
Ben Cooling <bencooling@users.noreply.github.com>
2018-03-24 14:31:22 +1000

    updated

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-# Auth
+# React Router 4 Auth Examples
 
-[onEnter prop in react-router v4](https://stackoverflow.com/a/45430656)
+## 1
+
+- 2 routes: protected and public. 
+- login/logout button.
+- protected redirects to public if not logged in.
+- protected fetches data from github api before rendering
+- Uses: withRouter, Route render.
+- Only works for browser session (can't type route in address bar/refresh page)
+
+
+## 2 (TODO)
+
+TODO:
+- Fetch data on two different routes, not just on login
+- Use [lifecycle hooks](https://stackoverflow.com/a/45430656)
+- Use [session cookies](https://httpbin.org)
+
+```
+componentWillMount() { // or componentDidMount
+  fetch(this.props.match.params.id)
+}
+
+componentWillReceiveProps(nextProps) { // or componentDidUpdate
+  if (nextProps.match.params.id !== this.props.match.params.id) {
+    fetch(nextProps.match.params.id)
+  }
+}
+```
+
+
+## resources
+
+- [onEnter prop in react-router v4](https://stackoverflow.com/a/45430656)
--- src/index.js
+++ src/index.js
@@ -1,41 +1,38 @@
-import React from "react";
-import { render } from "react-dom";
+import React from 'react'
+import { render } from 'react-dom'
 import {
   BrowserRouter,
   Route,
   Link,
   Redirect,
-  withRouter
-} from "react-router-dom";
+  withRouter,
+} from 'react-router-dom'
 
 const session = {
   active: false,
   repos: [],
   login(cb) {
-    this.active = true;
-    return fetch("https://api.github.com/users/bencooling/repos")
+    this.active = true
+    return fetch('https://api.github.com/users/bencooling/repos')
       .then(response => response.json())
-      .then(repos => (this.repos = repos));
-    // setTimeout(cb, 100); // fake async
+      .then(repos => (this.repos = repos))
   },
   logout(cb) {
-    this.active = false;
-    this.repos = [];
-    return Promise.resolve();
-  }
-};
+    this.active = false
+    this.repos = []
+    return Promise.resolve()
+  },
+}
 
-// <Redirect to={{ pathname: '/' }} /> // route object
-// history.push('/')
 const AuthButton = withRouter(({ history }) => {
-  const logout = () => session.logout().then(() => history.push("/"));
-  const login = () => session.login().then(() => history.push("/protected"));
+  const logout = () => session.logout().then(() => history.push('/'))
+  const login = () => session.login().then(() => history.push('/protected'))
   return session.active ? (
     <button onClick={logout}>logout</button>
   ) : (
     <button onClick={login}>login</button>
-  );
-});
+  )
+})
 
 const ProtectedRoute = ({ component: Component, ...rest }) => (
   <Route
@@ -44,13 +41,13 @@ const ProtectedRoute = ({ component: Component, ...rest }) => (
       session.active ? (
         <Component {...props} />
       ) : (
-        <Redirect to={{ pathname: "/public" }} />
+        <Redirect to={{ pathname: '/public' }} />
       )
     }
   />
-);
+)
 
-const Public = () => <h3>Public</h3>;
+const Public = () => <h3>Public</h3>
 const Protected = () => (
   <div>
     <h3>Ben Cooling Github repos</h3>
@@ -62,7 +59,7 @@ const Protected = () => (
       ))}
     </ul>
   </div>
-);
+)
 
 render(
   <BrowserRouter>
@@ -80,5 +77,5 @@ render(
       <ProtectedRoute path="/protected" component={Protected} />
     </div>
   </BrowserRouter>,
-  document.getElementById("root")
-);
+  document.getElementById('root'),
+)
